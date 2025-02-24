93fd2fbdf27735d1cefcdf0a9200a9c6ddd47bd5
GiuliaFliescher <64255332+GiuliaFliescher@users.noreply.github.com>
2025-02-24 14:21:31 +0100

    Add files via upload

---
+++ txt2double.m
@@ -0,0 +1,18 @@
+% Read the file
+filename = 'iqframes_2402_pt1.txt'; % Replace with your actual filename
+fid = fopen(filename, 'r');
+rawText = fread(fid, '*char')';
+fclose(fid);
+
+% Extract the IQ data using regular expressions
+iqPattern = '\((-?\d+\.\d+),(-?\d+\.\d+)\)'; % Pattern for extracting IQ pairs
+iqData = regexp(rawText, iqPattern, 'tokens');
+
+% Convert extracted data to a double array
+iqArray = cellfun(@(x) str2double(x), vertcat(iqData{:}));
+
+% Reshape into Nx2 matrix (I and Q components)
+iqframes_2402_pt1 = reshape(iqArray, [], 2);
+
+% Display the first few rows
+disp(iqframes_2402_pt1(1:10, :)); 
